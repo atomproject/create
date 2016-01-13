eb5553c0f1e78606aa0b492da6be0a041f72dc5e
Shailesh <ssrivastava@tavisca.com>
2016-01-13 11:18:24 +0530

    [FIX] redirection issue

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,6 +11,9 @@
     app.baseUrl = '/';
     app.menu = null;
     if (window.location.port === '') { // if production
+        // Uncomment app.baseURL below and
+        // set app.baseURL to '/your-pathname/' if running from folder in production
+         app.baseUrl = '/fusion/';
     }
 
     // Get elements for menu for a given category
